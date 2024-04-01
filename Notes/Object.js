9e4238ed965f174b2a454f84c10b40cee6802998
unknown <joesi8302@gmail.com>
2024-04-01 10:29:53 -0700

    addming more stuffs

---
+++ Notes/Object.js
@@ -0,0 +1,93 @@
+// Objects - Defined with class Object in JS are the back bone or basic allocation of JS
+// var User = new Object({}); // This is termed as Constructor based object creation
+
+// 1. Using two curly brackets
+var Employee = {
+    Name: "Nhan",
+    ID: 2123,
+    GroupCode: "Permanent",
+    GetEmployeeInfo: function(){
+        return this.Name + " " + this.ID + " " + this.GroupCode +  " " + this.Salary
+    }
+}
+
+console.log(Employee.GetEmployeeInfo());
+
+//Employee.Salary = "$25000" // this adds a new property to the class 
+
+console.log(Employee.GetEmployeeInfo());
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// THIS IS NOT RECCOMMENDED TO USE FOR INHERITANCE
+// THIS JUST CREATES 2 DIFFERENT COPIES
+// 2. We can use object contructor to create a copy and provide partial inheritance
+// var SoftwareEng = new Object(Employee);
+
+// SoftwareEng.Salary = "$30000" // new property
+
+// console.log(SoftwareEng.GetEmployeeInfo())
+
+// //Overriding - GetEmployeeInfo
+// SoftwareEng.GetEmployeeInfo = function(){
+//     return this.Name + " " + this.ID + " " + this.GroupCode +  " " + this.Salary
+// }
+
+// console.log(Employee.GetEmployeeInfo())
+// console.log(SoftwareEng.GetEmployeeInfo())
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// 3. We SHOULD use Object.create to create a copy and provide inheritance
+var SoftwareEng1 = Object.create(Employee);
+
+SoftwareEng1.Salary = "$30000" // new property
+
+console.log(SoftwareEng1.GetEmployeeInfo())
+
+//Overriding - GetEmployeeInfo
+SoftwareEng1.GetEmployeeInfo = function(){
+    return this.Name + " " + this.ID + " " + this.GroupCode +  " " + this.Salary
+}
+
+console.log(Employee.GetEmployeeInfo())
+console.log(SoftwareEng1.GetEmployeeInfo())
+
+// This inheritance in javascript is possible only due to prototype
+// prototype - is an object in JS to create link between child and parent
+
+console.log(SoftwareEng1.__proto__) // This will return employee object 
+//you can check on google chrome console to check the prototype connected to each object
+
+
+/////////////////////////////////////////////////////////////////////////////////
+// 4. Empty Object
+var EmptyObj = {} // new Object({})
+console.log(EmptyObj.__proto__) // We see that the prototype is "Object"
+
+
+
+// 5. Breaking the prototype chain and define base functions
+
+// Create your own prototype
+var nullPrototype = Object.create(null)
+
+nullPrototype.toString = function (params){
+    return "Does Something"
+}
+
+// 6. Object.Assign - merging two objects
+var User = {name: "Ailleen", add1: "Lake Ciry 1", mobile: "654321651"}
+var Address = {name : "Ailleen", add1: "Wall Strees", productName: "New procuict"}
+
+// var Delivery = {User, Address}
+// console.log(Delivery)
+
+
+var Delivery = Object.assign(User, Address)
+
+console.log(Delivery)
+
+//create one object with name person, and inherit it to Student and create two new properties and one new method
+//pls use both ways of inheritance
---
+++ Notes/literals.js
@@ -0,0 +1,35 @@
+// Literals represent memory allocation of some / few values
+
+
+//User 1
+var name = "Bryan"
+var age = 99
+var address = "Somewhere on earth"
+
+//User 2
+var name2 = "Gauri"
+var age2 = 98
+var address2 = "Somewhere in US"
+
+
+//Using Object Literals
+var User = {
+    name: "Wanda",
+    age: 26,
+    address: "Somewhere in Canada"
+}
+
+
+// When we need to read the value
+var readName = User.name;
+
+// When we need to update
+User.name = "David"
+
+
+var User2 = User;
+
+
+
+
+
